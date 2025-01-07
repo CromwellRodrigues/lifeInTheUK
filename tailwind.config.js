0ccc64e4af3a39248195b4699629211a2d694cab
daffodil009 <daffodil.luck9@gmail.com>
2025-01-07 07:40:16 +0000

    add data

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
-  content: [],
+  // purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
